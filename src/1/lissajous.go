df25207584b68f3dc19ec3dc982e5e2721fa8188
onlyonedaniel <1324013589@qq.com>
2022-03-08 22:57:11 +0800

    add fetch

--- src/1/lissajous.go
+++ src/1/lissajous.go
@@ -3,5 +3,24 @@ package main
 import(
 	"fmt"
 	"os"
-	""
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"time"
 )
+
+func main()  {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+}
+
+func lissajous(out io.Writer)  {
+	const {
+		cycles = 5
+		res = 0.001
+		size = 100
+		nframes = 64
+	}
+}
